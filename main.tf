2812792cd2bcdd2011db9b177d09b61f66a3d72c
Minh Hieu Nguyen <91326485+hieunm2411@users.noreply.github.com>
2025-06-03 15:53:49 +0700

    update main.tf

--- main.tf
+++ main.tf
@@ -19,26 +19,23 @@ data "aws_vpc" "default" {
 }
 
 resource "aws_instance" "blog" {
-  ami                     = data.aws_ami.app_ami.id
-  instance_type           = var.instance_type
-  key_name                = "n8n"
-
-  vpc_security_group_ids  = [module.blog_sg.security_group_id]
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
-    Name                  = "Learning Terraform"
+    Name = "Learning Terraform 1"
   }
 }
 
 module "blog_sg" {
-  source              = "terraform-aws-modules/security-group/aws"
-  version             = "4.13.0"
-  name                = "blog_new"
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
 
-  vpc_id              = data.aws_vpc.default.id
-  ingress_rules       = ["http-80-tcp","https_443_tcp"]
+  vpc_id  = data.aws_vpc.default.id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules        = ["all-all"]
+  egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
