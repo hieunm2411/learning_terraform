1f31f6d144872264cbdbddb3479df73b4ddc0da0
Minh Hieu Nguyen <91326485+hieunm2411@users.noreply.github.com>
2025-06-04 16:53:48 +0700

    update autoscaling

--- main.tf
+++ main.tf
@@ -69,36 +69,3 @@ module "blog_autoscaling" {
     Environment = "dev"
   }
 }
-
-module "blog_alb" {
-  source  = "terraform-aws-modules/alb/aws"
-
-  name = "blog-alb"
-
-  load_balancer_type = "application"
-
-  vpc_id             = module.blog_vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnets
-  security_groups    = [module.blog_sg.security_group_id]
-
-  listeners = {
-    ex-http-https-redirect = {
-      port     = 80
-      protocol = "HTTP"
-      target_group_index = 0
-    }
-  }
-
-  target_groups = [
-    {
-      name_prefix      = "blog-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-    }
-  ]
-
-  tags = {
-    Environment = "dev"
-  }
-}
