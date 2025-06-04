b160a35cf8880b13cce8e068b868583ac1ceb96b
Minh Hieu Nguyen <91326485+hieunm2411@users.noreply.github.com>
2025-06-04 11:47:42 +0700

    ok

--- main.tf
+++ main.tf
@@ -20,10 +20,9 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
+  azs             = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
+  
+  public_subnets  = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
 
   tags = {
     Terraform = "true"
@@ -31,20 +30,20 @@ module "blog_vpc" {
   }
 }
 
-
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "6.5.2"
+  version = "8.3.0"
+  
+  name      = "blog"
+  min_size  = 1
+  max_size  = 2
 
-  name = "blog"
+  vpc_zone_identifier   = module.blog_vpc.public_subnets
 
-  min_size            = 1
-  max_size            = 2
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
-  security_groups     = [module.blog_sg.security_group_id]
-  instance_type       = var.instance_type
-  image_id            = data.aws_ami.app_ami.id
+  image_id                = data.aws_ami.app_ami.id
+  instance_type           = var.instance_type
+
+  security_groups  = [module.blog_sg.security_group_id]
 }
 
 module "blog_alb" {
@@ -82,13 +81,14 @@ module "blog_alb" {
 }
 
 module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
+  source              = "terraform-aws-modules/security-group/aws"
+  version             = "4.13.0"
+  name                = "blog"
 
-  vpc_id  = module.blog_vpc.vpc_id
-  name    = "blog"
-  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  vpc_id              = module.blog_vpc.vpc_id
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules = ["all-all"]
+
+  egress_rules        = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
