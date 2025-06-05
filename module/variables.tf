db7b4fa6c959449d2d22346c7f9681aa3ef5b442
Minh Hieu Nguyen <91326485+hieunm2411@users.noreply.github.com>
2025-06-05 11:28:36 +0700

    Update variables.tf

--- module/variables.tf
+++ module/variables.tf
@@ -17,7 +17,7 @@ variable "ami_filter" {
 }
 
 variable "environment" {
-  description       = "Development environment"
+  description       = "Name of the environment"
   type              = object({
     name            = string
     network_prefix  = string
